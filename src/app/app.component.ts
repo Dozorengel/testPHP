cc56d8d9a27fc192712a9a927cab0e2dc4a933b2
Dozorengel <dozorengel@gmail.com>
2018-07-14 08:48:18 +0800

    coords and timestamp to console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html',  
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    console.log("x: " + event.pageX + ", y: " + event.pageY + ", ts: " + + new Date());
+  }
 }
