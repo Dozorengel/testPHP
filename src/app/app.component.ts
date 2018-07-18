d800fa65730e6d2fa95354a5d93765bbb23a2633
Dozorengel <dozorengel@gmail.com>
2018-07-18 19:53:31 +0800

    update from http to httpClient & get connected to DB via API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener } from '@angular/core';
-import { Http } from "@angular/http";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +7,26 @@ import { Http } from "@angular/http";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  constructor(private http: Http) { }
-
   title = 'app';
+
+  private url = 'http://localhost:8000/api/store';
+
+  constructor(private httpClient: HttpClient) { }
 
   @HostListener('document:click', ['$event'])
   clickout(event) {
-    //Отправляем на предполагаемый локальный сервер
-    this.http.post('http://localhost:8080/', {
+    var body = {
       x: event.pageX,
       y: event.pageX,
       ts: + new Date()
+    }
+    //Отправляем на локальный сервер
+    this.httpClient.post(this.url, body).subscribe(data => {
+      console.log(data);
     });
 
     //Для отладки
     console.log("x: " + event.pageX + ", y: " + event.pageY + ", ts: " + + new Date());
-    console.log({
-      x: event.pageX,
-      y: event.pageX,
-      ts: + new Date()
-    });
+    console.log(body);
   }
 }
